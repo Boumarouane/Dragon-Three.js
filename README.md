91f85cc53da658dc3b98cc9bd2fc7fab05ac535c
Boumarouane <i.boumarouane@gmail>
2020-03-30 09:04:41 +0200

    j'ai enfin compris comment casser les os d'un dragon *_*

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Dragon-Three.js
+
+Voici le lien : [https://boumarouane.github.io/Dragon-Three.js/](https://boumarouane.github.io/Dragon-Three.js/)
--- app.js
+++ app.js
@@ -11,15 +11,20 @@ let scene,
     neck4,
     neck5,
     head,
-    idle
+    idle;
 
 // Fonction contenant les initialisations
 let init = () => {
-    const canvas = document.querySelector('#dragon');
+    const canvas = document.querySelector("#dragon");
 
     scene = new THREE.Scene();
 
-    camera = new THREE.PerspectiveCamera(65, window.innerWidth / window.innerHeight, 0.1, 15000);
+    camera = new THREE.PerspectiveCamera(
+        65,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        30000
+    );
     camera.position.set(1800, -900, 1000);
 
     renderer = new THREE.WebGLRenderer({
@@ -31,72 +36,85 @@ let init = () => {
     document.body.appendChild(renderer.domElement);
 
     controls = new THREE.OrbitControls(camera, renderer.domElement);
-    controls.addEventListener('change', renderer);
-    controls.minDistance = 2000;
-    controls.maxDistance = 2800;
+    // controls.addEventListener("change", renderer);
+    // controls.minDistance = 4000;
+    // controls.maxDistance = 4000;
 
     let materialArray = [];
-    let texture_ft = new THREE.TextureLoader().load('paze_ft.jpg');
-    let texture_bk = new THREE.TextureLoader().load('paze_bk.jpg');
-    let texture_up = new THREE.TextureLoader().load('paze_up.jpg');
-    let texture_dn = new THREE.TextureLoader().load('paze_dn.jpg');
-    let texture_rt = new THREE.TextureLoader().load('paze_rt.jpg');
-    let texture_lf = new THREE.TextureLoader().load('paze_lf.jpg');
+    let texture_ft = new THREE.TextureLoader().load("paze_ft.jpg");
+    let texture_bk = new THREE.TextureLoader().load("paze_bk.jpg");
+    let texture_up = new THREE.TextureLoader().load("paze_up.jpg");
+    let texture_dn = new THREE.TextureLoader().load("paze_dn.jpg");
+    let texture_rt = new THREE.TextureLoader().load("paze_rt.jpg");
+    let texture_lf = new THREE.TextureLoader().load("paze_lf.jpg");
 
-    materialArray.push(new THREE.MeshBasicMaterial({
-        map: texture_ft
-    }));
-    materialArray.push(new THREE.MeshBasicMaterial({
-        map: texture_bk
-    }));
-    materialArray.push(new THREE.MeshBasicMaterial({
-        map: texture_up
-    }));
-    materialArray.push(new THREE.MeshBasicMaterial({
-        map: texture_dn
-    }));
-    materialArray.push(new THREE.MeshBasicMaterial({
-        map: texture_rt
-    }));
-    materialArray.push(new THREE.MeshBasicMaterial({
-        map: texture_lf
-    }));
+    materialArray.push(
+        new THREE.MeshBasicMaterial({
+            map: texture_ft
+        })
+    );
+    materialArray.push(
+        new THREE.MeshBasicMaterial({
+            map: texture_bk
+        })
+    );
+    materialArray.push(
+        new THREE.MeshBasicMaterial({
+            map: texture_up
+        })
+    );
+    materialArray.push(
+        new THREE.MeshBasicMaterial({
+            map: texture_dn
+        })
+    );
+    materialArray.push(
+        new THREE.MeshBasicMaterial({
+            map: texture_rt
+        })
+    );
+    materialArray.push(
+        new THREE.MeshBasicMaterial({
+            map: texture_lf
+        })
+    );
 
-    for (let i = 0; i < 6; i++)
-        materialArray[i].side = THREE.BackSide;
+    for (let i = 0; i < 6; i++) materialArray[i].side = THREE.BackSide;
     let skyboxGeo = new THREE.BoxGeometry(10000, 10000, 10000);
     let skybox = new THREE.Mesh(skyboxGeo, materialArray);
     scene.add(skybox);
 
     // Config loader
     loader = new THREE.GLTFLoader();
-    loader.load('./bless_wb/scene.gltf', function (object) {
-
+    loader.load("./bless_wb/scene.gltf", function(object) {
         let model = object.scene;
         model.traverse(o => {
             if (o.isBone) {
                 console.log(o.name);
             }
             // Reference the neck and waist bones
-            if (o.isBone && o.name === 'Bip01_Neck3_0140') {
+            if (o.isBone && o.name === "Bip01_Neck3_0140") {
                 neck3 = o;
             }
-            if (o.isBone && o.name === 'Bip01_Neck4_0141') {
+            if (o.isBone && o.name === "Bip01_Neck4_0141") {
                 neck4 = o;
             }
-            if (o.isBone && o.name === 'Bip01_Neck5_0142') {
+            if (o.isBone && o.name === "Bip01_Neck5_0142") {
                 neck5 = o;
             }
-            if (o.isBone && o.name === 'Bip01_Head_0143') {
+            if (o.isBone && o.name === "Bip01_Head_0143") {
                 head = o;
             }
         });
-        model.position.set(0, -1000, 0)
+        model.position.set(0, -1000, 0);
         scene.add(model);
 
         animation = new THREE.AnimationMixer(model);
 
-        let action = THREE.AnimationClip.findByName(object.animations, "CINEMA_4D_Main");
+        let action = THREE.AnimationClip.findByName(
+            object.animations,
+            "CINEMA_4D_Main"
+        );
         console.log(action);
         // Add these:
         action.tracks.splice(181, 10);
@@ -107,41 +125,47 @@ let init = () => {
     // Config light
     light = new THREE.AmbientLight(0xffffff, 2);
     scene.add(light);
-}
+};
 
 let animate = () => {
-
-    let delta = clock.getDelta()
+    let delta = clock.getDelta();
     if (animation) animation.update(delta);
 
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
-}
+};
 
-document.addEventListener('mousemove', function (e) {
+// Event qui permet le déplacement de la tete
+document.addEventListener("mousemove", function(e) {
     var mousecoords = getMousePos(e);
     if (neck3 && neck4 && head && neck5) {
         moveJoint(mousecoords, neck3, 10);
         moveJoint(mousecoords, neck4, 10);
-        moveJoint(mousecoords, neck5, -10);
-        moveJoint(mousecoords, head, -20);
+        moveJoint(mousecoords, neck5, 20);
+        moveJoint(mousecoords, head, -30);
     }
 });
 
-function getMousePos(e) {
+// Fonction qui renvoie la position de notre souris chaque fois qu'elle est déplacée
+let getMousePos = e => {
     return {
         x: e.clientX,
         y: e.clientY
     };
-}
+};
 
-function moveJoint(mouse, joint, degreeLimit) {
+// La fonction moveJoint() prend trois arguments, la position actuelle de la souris, l'articulation que nous voulons déplacer et la limite (en degrés)de l'articulation
+let moveJoint = (mouse, joint, degreeLimit) => {
+    // Fonction getMouseDegrees() qui retourne un objet de {x, y} => en deg
     let degrees = getMouseDegrees(mouse.x, mouse.y, degreeLimit);
+    // On utilise ensuite ces degrés pour faire pivoter le joint sur l'axe x et l'axe y
     joint.rotation.y = THREE.Math.degToRad(degrees.x);
     joint.rotation.x = THREE.Math.degToRad(degrees.y);
-}
+};
 
-function getMouseDegrees(x, y, degreeLimit) {
+// La fonction getMouseDegrees() détermine où la souris se trouve sur l'écran dans un pourcentage entre le milieu et chaque bord de l'écran. Une fois que la fonction a ces pourcentages, elle renvoie le pourcentage du degreeLimit.
+let getMouseDegrees = (x, y, degreeLimit) => {
+    // Les variables dont on a besoin
     let dx = 0,
         dy = 0,
         xdiff,
@@ -154,42 +178,46 @@ function getMouseDegrees(x, y, degreeLimit) {
         y: window.innerHeight
     };
 
-    // Left (Rotates neck left between 0 and -degreeLimit)
-
-    // 1. If cursor is in the left half of screen
+    // ! Gauche (fait pivoter le col à gauche entre 0 et -degreeLimit)
+    // 1. Si le curseur se trouve dans la moitié gauche de l'écran
     if (x <= w.x / 2) {
-        // 2. Get the difference between middle of screen and cursor position
+        // 2. Obtenir la différence entre le milieu de l'écran et la position du curseur
         xdiff = w.x / 2 - x;
-        // 3. Find the percentage of that difference (percentage toward edge of screen)
+
+        // 3. trouvez le pourcentage de cette différence (pourcentage vers le bord de l'écran)
         xPercentage = (xdiff / (w.x / 2)) * 100;
-        // 4. Convert that to a percentage of the maximum rotation we allow for the neck
+
+        // 4. convertissez cela en pourcentage de la rotation maximale que nous autorisons pour le cou
         dx = ((degreeLimit * xPercentage) / 100) * -1;
     }
-    // Right (Rotates neck right between 0 and degreeLimit)
+
+    //! Droite (fait tourner le cou à droite entre 0 et degreeLimit)
     if (x >= w.x / 2) {
         xdiff = x - w.x / 2;
         xPercentage = (xdiff / (w.x / 2)) * 100;
         dx = (degreeLimit * xPercentage) / 100;
     }
-    // Up (Rotates neck up between 0 and -degreeLimit)
+
+    //! Vers le haut (Rotation du cou vers le haut entre 0 et -degreeLimite)
     if (y <= w.y / 2) {
         ydiff = w.y / 2 - y;
         yPercentage = (ydiff / (w.y / 2)) * 100;
-        // Note that I cut degreeLimit in half when she looks up
-        dy = (((degreeLimit * 0.5) * yPercentage) / 100) * -1;
+        // Notez que je coupe le degreeLimit en deux quand elle lève les yeux
+        dy = ((degreeLimit * 0.5 * yPercentage) / 100) * -1;
     }
 
-    // Down (Rotates neck down between 0 and degreeLimit)
+    //! Bas (Tourne le cou vers le bas entre 0 et degreeLimite)
     if (y >= w.y / 2) {
         ydiff = y - w.y / 2;
         yPercentage = (ydiff / (w.y / 2)) * 100;
-        dy = (degreeLimit * yPercentage) / 100;
+        dy = (degreeLimit * 0.2 * yPercentage) / 100;
     }
+
     return {
         x: dx,
         y: dy
     };
-}
+};
 
 init();
 animate();
